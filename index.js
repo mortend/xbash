7a4b8493640be9b9dc7e078070c5cd64bd634743
Morten Daniel Fornes <mdfornes@gmail.com>
2019-08-03 05:00:08 +0700

    extract findGitForWindowsExe()

--- index.js
+++ index.js
@@ -3,73 +3,27 @@ const path = require('path');
 const which = require('which');
 const {spawn} = require('child_process');
 
-function error(name) {
+function findGitForWindowsExe(name, dir) {
+    let exe = null;
+    let git = which.sync('git', {nothrow: true});
+
+    if (git) {
+        exe = path.join(path.dirname(path.dirname(git)), dir, 'bin', name);
+        if (fs.existsSync(exe))
+            return exe;
+    }
+
+    exe = path.join(process.env.PROGRAMFILES, 'Git', dir, 'bin', name);
+    if (fs.existsSync(exe))
+        return exe;
+
+    exe = which.sync(name, {nothrow: true});
+    if (exe)
+        return exe;
+
     console.error('ERROR: \'' + name + '\' was not found. This can be solved by installing Git.')
     console.error("\nPlease get Git from https://git-scm.com/downloads and try again.");
     process.exit(1);
-}
-
-function findBashForWindows() {
-    let bash = null;
-    let git = which.sync('git', {nothrow: true});
-
-    if (git) {
-        bash = path.join(path.dirname(path.dirname(git)), 'usr', 'bin', 'bash.exe');
-        if (fs.existsSync(bash))
-            return bash;
-    }
-    
-    bash = path.join(process.env.PROGRAMFILES, 'Git', 'usr', 'bin', 'bash.exe');
-    if (fs.existsSync(bash))
-        return bash;
-
-    bash = which.sync('bash', {nothrow: true});
-    if (bash)
-        return bash;
-
-    error('bash');
-}
-
-function findCurlForWindows() {
-    let curl = null;
-    let git = which.sync('git', {nothrow: true});
-
-    if (git) {
-        curl = path.join(path.dirname(path.dirname(git)), 'mingw', 'bin', 'curl.exe');
-        if (fs.existsSync(curl))
-            return curl;
-    }
-
-    curl = path.join(process.env.PROGRAMFILES, 'Git', 'mingw', 'bin', 'curl.exe');
-    if (fs.existsSync(curl))
-        return curl;
-
-    curl = which.sync('curl', {nothrow: true});
-    if (curl)
-        return curl;
-
-    error('curl');
-}
-
-function findUnzipForWindows() {
-    let unzip = null;
-    let git = which.sync('git', {nothrow: true});
-
-    if (git) {
-        unzip = path.join(path.dirname(path.dirname(git)), 'usr', 'bin', 'unzip.exe');
-        if (fs.existsSync(bash))
-            return bash;
-    }
-    
-    unzip = path.join(process.env.PROGRAMFILES, 'Git', 'usr', 'bin', 'unzip.exe');
-    if (fs.existsSync(bash))
-        return bash;
-
-    unzip = which.sync('unzip', {nothrow: true});
-    if (unzip)
-        return unzip;
-
-    error('unzip');
 }
 
 let bash = 'bash';
@@ -77,17 +31,17 @@ let curl = 'curl';
 let unzip = 'unzip';
 
 if (path.sep == '\\') {
-    bash = findBashForWindows();
+    bash = findGitForWindowsExe('bash.exe', 'usr');
     process.env.PATH = path.dirname(bash)
         .concat(path.delimiter, process.env.PATH);
 
     // Make sure we have curl in PATH inside bash.
-    curl = findCurlForWindows();
+    curl = findGitForWindowsExe('curl.exe', 'mingw');
     process.env.PATH = path.dirname(curl)
         .concat(path.delimiter, process.env.PATH);
 
     // Make sure we have unzip in PATH inside bash.
-    unzip = findUnzipForWindows();
+    unzip = findGitForWindowsExe('unzip.exe', 'usr');
     process.env.PATH = path.dirname(unzip)
         .concat(path.delimiter, process.env.PATH);
 }
